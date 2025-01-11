319a73458ac69646290ed1dd3d74bdb066d0f07a
aditya90205 <adityasrivastav90205@gmail.com>
2025-01-12 00:19:16 +0530

    Initial Commit with Home Page Setup

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+type Props = {
+  children: React.ReactNode
+}
+
+const layout = ({children}: Props) => {
+  return (
+    <main className="root-container">
+      <div className="mx-auto max-w-7xl">
+        <Header />
+
+        <div className="mt-20 pb-20">{children}</div>
+      </div>
+    </main>
+  )
+}
+
+export default layout
