e5d8285ea7ccfbc8dd69d926f1cd19361cfd29a4
aditya90205 <adityasrivastav90205@gmail.com>
2025-01-13 13:11:01 +0530

    Complete SignUp and SignIn page

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const layout = ({ children }: Props) => {
+  return (
+    <main className="auth-container">
+      <section className="auth-form">
+        <div className="auth-box">
+          <div className="flex flex-row gap-3">
+            <Image src="/icons/logo.svg" alt="logo" width={37} height={37} />
+            <h1 className="text-2xl font-semibold text-white">BookWise</h1>
+          </div>
+
+          <div>{children}</div>
+        </div>
+      </section>
+
+      <section className="auth-illustration">
+        <Image
+          src="/images/auth-illustration.png"
+          alt="auth-illustration"
+          width={1000}
+                  height={1000}
+                  className="size-full object-cover"
+        />
+      </section>
+    </main>
+  );
+};
+
+export default layout;
---
+++ components/ImageUpload.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ImageUpload = (props: Props) => {
+  return (
+    <div>ImageUpload</div>
+  )
+}
+
+export default ImageUpload
